e09719c5344cd7512da1fc4b5a07ea76ceb03392
Jesse Duffield <jessedduffield@gmail.com>
2022-10-08 13:35:25 -0700

    add box layout

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/jesseduffield/lazycore
+
+go 1.18
+
+require (
+	github.com/jesseduffield/generics v0.0.0-20220320043834-727e535cbe68
+	github.com/samber/lo v1.31.0
+	github.com/stretchr/testify v1.8.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20220317015231-48e79f11773a // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ pkg/utils/utils_test.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestMin is a function.
+func TestMin(t *testing.T) {
+	type scenario struct {
+		a        int
+		b        int
+		expected int
+	}
+
+	scenarios := []scenario{
+		{
+			1,
+			1,
+			1,
+		},
+		{
+			1,
+			2,
+			1,
+		},
+		{
+			2,
+			1,
+			1,
+		},
+	}
+
+	for _, s := range scenarios {
+		assert.EqualValues(t, s.expected, Min(s.a, s.b))
+	}
+}
