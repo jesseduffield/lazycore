c2900692cd19d2e2faac37888bb9d41734cf3f5c
Gustavo Andrioli <gusaandrioli@gmail.com>
2022-10-11 22:16:22 -0300

    Add Clamp test

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -9,9 +9,9 @@ import (
 // TestMin is a function.
 func TestMin(t *testing.T) {
 	type scenario struct {
-		a        int
-		b        int
-		expected int
+		a    int
+		b    int
+		want int
 	}
 
 	scenarios := []scenario{
@@ -33,7 +33,45 @@ func TestMin(t *testing.T) {
 	}
 
 	for _, s := range scenarios {
-		assert.EqualValues(t, s.expected, Min(s.a, s.b))
+		assert.EqualValues(t, s.want, Min(s.a, s.b))
+	}
+}
+
+func TestClamp(t *testing.T) {
+	tests := []struct {
+		name string
+		x    int
+		min  int
+		max  int
+		want int
+	}{
+		{
+			"success",
+			5,
+			1,
+			10,
+			5,
+		},
+		{
+			"success",
+			-5,
+			1,
+			10,
+			1,
+		},
+		{
+			"success",
+			15,
+			1,
+			10,
+			10,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, Clamp(tt.x, tt.min, tt.max))
+		})
 	}
 }
 
